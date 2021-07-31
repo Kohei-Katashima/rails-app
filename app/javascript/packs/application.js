6c5ab542f31a7df432d051b73a5ad7ad044700b1
Kohei-Katashima <kkohei0325@gmail.com>
2021-07-31 11:37:46 +0900

    flashメッセージ、toast

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -9,6 +9,8 @@ import * as ActiveStorage from "@rails/activestorage"
 import "channels"
 import 'bootstrap';
 import '../stylesheets/application';
+import toastr from 'toastr'
+window.toastr = toastr
 
 Rails.start()
 Turbolinks.start()
